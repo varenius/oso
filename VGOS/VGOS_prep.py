011727874fbc6ff00d82c08d94e4f240dc0b444e
VLBI Operator <oper@rigel.gsfc.nasa.gov>
2019-09-03 12:26:47 +0000

    Added info output to script

--- VGOS/VGOS_prep.py
+++ VGOS/VGOS_prep.py
@@ -16,8 +16,10 @@ ftpaddr = "ftp://ivs.bkg.bund.de/pub/vlbi/ivsdata/aux/" + year + "/" + exp + "/"
 check = raw_input("Ready to fetch, drudg, and modify (SNP/PRC) experiment " + exp + ". NOTE: this will overwrite any existing files with this experiment name. Type go and hit enter to continue: " )
 if check.strip() == "go":
     # Get schedule via wget, saving it in /usr2/sched/, e.g. /usr2/sched/vt9248.skd
+    print("INFO: Downloading sked file...")
     wgetcmd = "wget --user anonymous " + ftpaddr + " -O /usr2/sched/" + exp + ".skd"
     os.system(wgetcmd)
+    print("INFO: ...done.")
     
     # get hostname of this FS machine, fulla or freja
     host = os.uname()[1]
@@ -27,19 +29,26 @@ if check.strip() == "go":
     # TODO: Could also read location.ctl if setup properly
     
     # drudg sked file for SNP. 
+    print("INFO: host is " + host + " so running drudg for telescope " + tel + " ...")
     drudgcmd = "drudg /usr2/sched/" + exp + ".skd " + tel + " 3 0" 
     os.system(drudgcmd)
+    print("INFO: ...done.")
     
     # change setupsx to setupbb in SNP file
+    print("INFO: Changing setupsx to setupbb in snp file...")
     sedcmd = "sed -i 's/setupsx/setupbb/g' /usr2/sched/"+exp+tel+".snp"
     os.system(sedcmd)
+    print("INFO: ...done.")
     
     # copy template PRC file to /usr2/proc/expST.prc where ST is oe or ow
+    print("INFO: Instead of drudging for PRC, copy template PRC...")
     cpcmd = "cp " + scriptpath + "/VGOS_default_prc." + tel + " /usr2/proc/" + exp + tel + ".prc"
     os.system(cpcmd)
 
     # change VT9248 to actual experiment name in PRC header
+    print("INFO: ... and edit the PRC header to use actual experiment namt...")
     sedcmd = "sed -i 's/VT9248/" + exp.upper() + "/g' /usr2/proc/"+exp+tel+".prc"
     os.system(sedcmd)
+    print("INFO: ...done.")
 else: 
     print("Did not get go as answer so not doing anything.")
