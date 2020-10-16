995419e40dd472a84249069a9c410a831f112de6
oper <oper@gyller.oso.chalmers.se>
2020-10-16 07:39:09 +0200

    Exp and antenna in listexp

--- MISC/listexp.py
+++ MISC/listexp.py
@@ -5,7 +5,8 @@ infile = "tmp.dat"
 exps = []
 for line in open(infile):
     if line.startswith("d"):
-        exp=line.split()[7].split("_")[0]
+        sp = line.split()[7].split("_")
+        exp=sp[0] + "_" + sp[1]  # Exp and antenna
         if not exp in exps:
             exps.append(exp)
 print("Found these VBS-experiments on disk:")
