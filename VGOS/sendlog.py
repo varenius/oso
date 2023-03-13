065a51ea58c0aa19905577c172a200cecf11651a
FS Operator <oper@njord.oso.chalmers.se>
2023-03-13 14:02:30 +0000

    ssl with error logging

--- VGOS/sendlog.py
+++ VGOS/sendlog.py
@@ -2,6 +2,8 @@
 #-*- coding: utf-8 -*-
 import os
 
-cmd = "ncftpput -u ivsincoming -p PASSWORD ivs.bkg.bund.de / /usr2/log/`lognm`.log"
+#cmd = "ncftpput -u ivsincoming -p PASSWORD ivs.bkg.bund.de / /usr2/log/`lognm`.log"
+# Changed by Eskil 2022-08-18
+cmd = "lftp -c 'set ftp:ssl-force true; open -u BKGPASSWORD ivs.bkg.bund.de; put -a /usr2/log/`lognm`.log; close; bye;' >> /usr2/log/lftp_BKG.log 2>&1"
 
 os.system(cmd)
