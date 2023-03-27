14895ea4358aee83f437748666d43cc8ede0a6ea
FS Operator <oper@njord.oso.chalmers.se>
2023-03-27 06:28:22 +0000

    Had forgotten debug statement to get output in log

--- VGOS/sendlog.py
+++ VGOS/sendlog.py
@@ -6,8 +6,9 @@ import sys
 # Give logfile path as argument, e.g.
 # sendlog.py /usr2/log/vo3068oe.log
 fslog = sys.argv[1]
-bkgpassword = "ENTER_HERE"
+bkgpassword = "ENTERPASS"
 print("Sending logfile {}...".format(fslog))
-cmd = "lftp -c 'set ftp:ssl-force true; open -u {} ivs.bkg.bund.de; put -a {}; close; bye;' >> /usr2/log/lftp_BKG.log 2>&1".format(bkgpassword, fslog)
+cmd = "lftp -c 'debug -t 9; set ftp:ssl-force true; open -u {} ivs.bkg.bund.de; put -a {}; close; bye;' >> /usr2/log/lftp_BKG.log 2>&1".format(bkgpassword, fslog)
 os.system(cmd)
+print(cmd)
 print("...done!")
